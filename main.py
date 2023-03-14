125f96bb453294f864e861ebcfb86fc01c964c46
Kristoffermg <kristoffermoeller151@gmail.com>
2023-03-14 14:17:49 +0100

    Temporary model
    R2 = 0.06
    MSE = 72.18
    MAE = 6.70

--- main.py
+++ main.py
@@ -1,42 +1,64 @@
-# This program takes the average electrical load of the high load values and plots it with the corresponding oil temperature
-# The program helps give a visual understanding of how the electrical load and oil temperature affect each other
-
-import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error     
+
+data = pd.read_csv("ETTh1.csv")
+
+# Makes the date on the X-axis more readable
+##data.index = pd.to_datetime(data.index)
+
+# Marks the last 3 months of the dataset as a test set
+##train = data.loc[data.index < '2018-03-26 23:00:00']
+##test = data.loc[data.index >= '2018-03-26 23:00:00']
+
+# Marks the training and test data with different colors
+##fig, ax = plt.subplots(figsize=(15,5))
+##train.plot(ax=ax, label='Training Set')
+##test.plot(ax=ax, label='Test Set')
+
+
+#separate the other attributes from the predicting attribute
+x = data.drop(['OT', 'date'], axis=1)
+#print(x)
+
+#separate the predicting attribute into Y for model training 
+y = data['OT']
+
+#data['OT'].plot(kind = 'line')
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
 
 
-dataframe = pd.read_csv("ETTh1.csv")
+LRmodel = LinearRegression()
+LRmodel.fit(x_train, y_train)
 
-arr = np.array([dataframe["HUFL"], dataframe["HULL"], dataframe["MUFL"], dataframe["MULL"], dataframe["LUFL"], dataframe["LULL"]])
-arrMean = np.mean(arr)
-
-HUFL = np.array(dataframe["HUFL"])
-HULL = np.array(dataframe["HULL"])
-MUFL = np.array(dataframe["MUFL"])
-MULL = np.array(dataframe["MULL"])
-LUFL = np.array(dataframe["LUFL"])
-LULL = np.array(dataframe["LULL"])
-OT = np.array(dataframe["OT"])
-
-datapointsRange = 500
-startPoint = 2000
-
-oilTemp = [0 for i in range(datapointsRange)]
-meanLoad = [0 for i in range(datapointsRange)]
-for x in range(datapointsRange):
-    #meanLoad[x] = (HUFL[x+startPoint] + HULL[x+startPoint] + MUFL[x+startPoint] + MULL[x+startPoint] + LUFL[x+startPoint] + LULL[x+startPoint]) / 6
-    meanLoad[x] = (HUFL[x+startPoint] + MUFL[x+startPoint] + LUFL[x+startPoint]) / 3
-    oilTemp[x] = OT[x+startPoint]
+y_prediction = LRmodel.predict(x_test)
 
 
-data = {'Load':meanLoad, 'Oil':oilTemp}
+plt.scatter(range(len(y_test)), y_test, color='blue')
+plt.scatter(range(len(y_prediction)), y_prediction, color='red')
 
 
-df = pd.DataFrame(data)
+
+score = r2_score(y_test, y_prediction)
+print('r2 score: ', score)
+print('MSE: ', mean_squared_error(y_test, y_prediction))
+print('MAE: ', mean_absolute_error(y_test, y_prediction))
 
 
-plt.plot(df)
+# Adds a line between the training and test data
+##ax.axvline('2018-03-26 23:00:00', color='black', ls='--')
+##ax.legend(['Training Set', 'Test Set'])
+
+#data.plot(style='.', figsize=(15,5), title='weewoo')
 
 plt.show()
+
+
