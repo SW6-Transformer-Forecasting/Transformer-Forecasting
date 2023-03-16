8ba2f1ded5e40b2a2d6c57592efe0945989a7882
Kristoffermg <33570390+Kristoffermg@users.noreply.github.com>
2023-03-16 12:12:15 +0100

    Much better predictions
    doesnt train on the whole model, only a very brief period. Has a much lower MSE than previous attempts

--- main.py
+++ main.py
@@ -7,58 +7,96 @@ from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error     
 
+from sklearn import linear_model
+
+
 data = pd.read_csv("ETTh1.csv")
 
-# Makes the date on the X-axis more readable
-##data.index = pd.to_datetime(data.index)
 
-# Marks the last 3 months of the dataset as a test set
-##train = data.loc[data.index < '2018-03-26 23:00:00']
-##test = data.loc[data.index >= '2018-03-26 23:00:00']
-
-# Marks the training and test data with different colors
-##fig, ax = plt.subplots(figsize=(15,5))
-##train.plot(ax=ax, label='Training Set')
-##test.plot(ax=ax, label='Test Set')
-
-
-#separate the other attributes from the predicting attribute
-x = data.drop(['OT', 'date'], axis=1)
-#print(x)
-
-#separate the predicting attribute into Y for model training 
+x = data[['HUFL', 'HULL', 'MUFL','MULL','LUFL','LULL']]
 y = data['OT']
 
-#data['OT'].plot(kind = 'line')
-
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
 
 
-LRmodel = LinearRegression()
-LRmodel.fit(x_train, y_train)
 
-y_prediction = LRmodel.predict(x_test)
+trainPeriod = data[(data['date'] < '2018-01-31 23:00:00') & (data['date'] >= '2018-01-01 23:00:00')]
 
+x_train = trainPeriod[['HUFL', 'HULL', 'MUFL','MULL','LUFL','LULL']]
+y_train = trainPeriod['OT']
+
+print(x_train)
+print(y_train)
+
+testPeriod = data.loc[(data['date'] >= '2018-02-01 23:00:00') & (data['date'] <= '2018-02-08 23:00:00')]
+x_test = testPeriod[['HUFL', 'HULL', 'MUFL','MULL','LUFL','LULL']]
+y_test = testPeriod['OT']
+
+
+regr = linear_model.LinearRegression()
+regr.fit(x_train, y_train)
+
+predictedOT = regr.predict(x_test)
+
+
+
+# for i in range(len(predictedOT)):
+#     f.write("Actual value: " + x_test[i] + "Predicted value: " + predictedOT[i])
 
 plt.scatter(range(len(y_test)), y_test, color='blue')
-plt.scatter(range(len(y_prediction)), y_prediction, color='red')
+plt.scatter(range(len(predictedOT)), predictedOT, color='red')
 
 
 
-score = r2_score(y_test, y_prediction)
-print('r2 score: ', score)
-print('MSE: ', mean_squared_error(y_test, y_prediction))
-print('MAE: ', mean_absolute_error(y_test, y_prediction))
-
-
-# Adds a line between the training and test data
-##ax.axvline('2018-03-26 23:00:00', color='black', ls='--')
-##ax.legend(['Training Set', 'Test Set'])
-
-#data.plot(style='.', figsize=(15,5), title='weewoo')
+print('MSE: ', mean_squared_error(y_test, predictedOT))
+print('MAE: ', mean_absolute_error(y_test, predictedOT))
 
 plt.show()
+
+
+
+
+
+
+
+
+# #separate the other attributes from the predicting attribute
+# x = data.drop(['OT', 'date'], axis=1)
+# #print(x)
+
+# #separate the predicting attribute into Y for model training 
+# y = data['OT']
+
+# #data['OT'].plot(kind = 'line')
+
+
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+
+
+
+# LRmodel = LinearRegression()
+# LRmodel.fit(x_train, y_train)
+
+# y_prediction = LRmodel.predict(x_test)
+
+
+# plt.scatter(range(len(y_test)), y_test, color='blue')
+# plt.scatter(range(len(y_prediction)), y_prediction, color='red')
+
+
+
+# score = r2_score(y_test, y_prediction)
+# print('r2 score: ', score)
+# print('MSE: ', mean_squared_error(y_test, y_prediction))
+# print('MAE: ', mean_absolute_error(y_test, y_prediction))
+
+
+# # Adds a line between the training and test data
+# ##ax.axvline('2018-03-26 23:00:00', color='black', ls='--')
+# ##ax.legend(['Training Set', 'Test Set'])
+
+# #data.plot(style='.', figsize=(15,5), title='weewoo')
+
+# plt.show()
 
 
