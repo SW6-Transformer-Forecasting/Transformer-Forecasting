69f65e9863b4d3812818b47decb92da3660ed4e1
Kristoffermg <kristoffermoeller151@gmail.com>
2023-03-30 12:54:12 +0200

    Linear predictions on OT with date
    -> Produces very small errors when using clean data, especially over a short period
    
    1 Month training, 6 hours predictions: - MSE 0.309
    - MAE 0.515
    
    1 month training, 1 day predictions:
    - MSE 0.493
    - MAE 0.570

--- main.py
+++ main.py
@@ -1,108 +1,37 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import csv
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error 
 from sklearn import linear_model
-from sklearn.preprocessing import StandardScaler
-
 
 data = pd.read_csv("ETTh1.csv")
+data['date'] = pd.to_datetime(data['date'])
 
+# Adds an extra column to the dataframe that displays the number of hours since the start of the data
+data['date_delta'] = (data['date'] - data['date'].min()) / np.timedelta64(1, 'h')
 
-# trainPeriod = data[(data['date'] <= '2018-01-08 23:00:00') & (data['date'] >= '2018-01-01 23:00:00')]
-trainPeriod = pd.read_csv("trainPeriod.csv")
-testPeriod = pd.read_csv("testPeriod.csv")
+trainPeriod = data[(data['date'] >= '2017-12-01 00:00:00') & (data['date'] < '2017-12-31 00:00:00')]
+testPeriod = data.loc[(data['date'] >= '2018-01-01 00:00:00') & (data['date'] <= '2018-01-01 06:00:00')]
 
+x_train = np.array(trainPeriod['date_delta'])
+y_train = np.array(trainPeriod['OT'])
 
-x_train = trainPeriod['date']
-y_train = trainPeriod['OT']
+x_test = np.array(testPeriod['date_delta'])
+y_test = np.array(testPeriod['OT'])
 
-
-x_test = testPeriod['date']
-y_test = testPeriod['OT']
-
-for date in x_train, :
-    print(date)
-
-# print(x_train)
-
-scaler = StandardScaler()
-# x_train_scaled = scaler.fit_transform(x_train)
-
-testPeriod = data.loc[(data['date'] > '2018-01-08 23:00:00') & (data['date'] <= '2018-01-09 23:00:00')]
-x_test = testPeriod[['HUFL', 'HULL', 'MUFL','MULL','LUFL', 'LULL']]
-y_test = testPeriod['OT']
-
-# x_test_scaled = scaler.fit_transform(x_test)
+print(trainPeriod)
 
 regr = linear_model.LinearRegression()
-# regr.fit(x_train, y_train)
+regr.fit(x_train.reshape(-1,1), y_train.reshape(-1,1))
 
-# predictedOT = regr.predict(x_test)
+predictedOT = regr.predict(x_test.reshape(-1,1))
 
-
-
-# for i in range(len(predictedOT)):
-#     f.write("Actual value: " + x_test[i] + "Predicted value: " + predictedOT[i])
-
-plt.scatter(range(len(y_test)), y_test, color='blue')
-# plt.scatter(range(len(predictedOT)), predictedOT, color='red')
-
-
-# print('MSE: ', mean_squared_error(y_test, predictedOT))
-# print('MAE: ', mean_absolute_error(y_test, predictedOT))
-
-# plt.show()
+print('MSE: ', mean_squared_error(y_test, predictedOT))
+print('MAE: ', mean_absolute_error(y_test, predictedOT))
 
 
 
 
 
-
-
-
-# #separate the other attributes from the predicting attribute
-# x = data.drop(['OT', 'date'], axis=1)
-# #print(x)
-
-# #separate the predicting attribute into Y for model training 
-# y = data['OT']
-
-# #data['OT'].plot(kind = 'line')
-
-
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
-
-
-
-# LRmodel = LinearRegression()
-# LRmodel.fit(x_train, y_train)
-
-# y_prediction = LRmodel.predict(x_test)
-
-
-# plt.scatter(range(len(y_test)), y_test, color='blue')
-# plt.scatter(range(len(y_prediction)), y_prediction, color='red')
-
-
-
-# score = r2_score(y_test, y_prediction)
-# print('r2 score: ', score)
-# print('MSE: ', mean_squared_error(y_test, y_prediction))
-# print('MAE: ', mean_absolute_error(y_test, y_prediction))
-
-
-# # Adds a line between the training and test data
-# ##ax.axvline('2018-03-26 23:00:00', color='black', ls='--')
-# ##ax.legend(['Training Set', 'Test Set'])
-
-# #data.plot(style='.', figsize=(15,5), title='weewoo')
-
-# plt.show()
 
 
