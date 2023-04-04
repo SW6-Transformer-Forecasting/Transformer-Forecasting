5ee3b56ec9ab4932564b9781d590364273482113
Archdragon <plaursen7.pl@gmail.com>
2023-04-04 10:07:29 +0200

    datafilter now contains the date and time in the filtered range

--- dataFilter.py
+++ dataFilter.py
@@ -5,7 +5,7 @@ class DataFilter():
     def fetch(self, filename, fromDate, toDate):
         data = pd.read_csv("{0}".format(filename))
         dataRange = data[(data['date'] <= '{0} 23:00:00'.format(toDate)) & (data['date'] >= '{0} 23:00:00'.format(fromDate))]
-        sortedData = dataRange[['HUFL', 'HULL', 'MUFL', 'MULL','LUFL', 'LULL', 'OT']]
+        sortedData = dataRange[['date', 'HUFL', 'HULL', 'MUFL', 'MULL','LUFL', 'LULL', 'OT']]
         return sortedData
     
     def filterData(self, data):
@@ -13,7 +13,7 @@ class DataFilter():
         data = data.to_numpy()
         
         appendFlag = True
-        prevAxis = [0] * 7 # Init of array filled with 0's to avoid double line data
+        prevAxis = [0] * 8 # Init of array filled with 0's to avoid double line data. Multiplier is amount of items in each row
         arr = []
         
         for row in data: 
@@ -37,13 +37,14 @@ class DataFilter():
             appendFlag = True
             
             
-        arr = np.split(arr, len(arr)/7)
+        arr = np.split(arr, len(arr)/8)
         return arr
     
     def saveFile(self, data):
-        df = pd.DataFrame(data, columns=['HUFL', 'HULL', 'MUFL', 'MULL', 'LUFL', 'LULL', 'OT'])
+        df = pd.DataFrame(data, columns=['date', 'HUFL', 'HULL', 'MUFL', 'MULL', 'LUFL', 'LULL', 'OT'])
         df.to_csv('cleandata.csv', index=False)
 
     def execute(self, data):
         dfilter = DataFilter()
         dfilter.saveFile(dfilter.filterData(data))
+        
