6ab80eeccb884c259ec484cd67c3a27e669b4a67
Kristoffermg <33570390+Kristoffermg@users.noreply.github.com>
2023-04-06 13:14:56 +0200

    SQL setup
    -> MySQL connection string
    -> Data access class

---
+++ Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/SQL/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace kristofferhusdata.DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public async Task<List<T>> LoadData<T, U>(string query, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(query, parameters);
+
+                return rows.ToList();
+            }
+        }
+    }
+}
