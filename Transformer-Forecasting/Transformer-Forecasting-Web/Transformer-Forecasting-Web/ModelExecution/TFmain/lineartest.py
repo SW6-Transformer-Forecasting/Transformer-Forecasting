4419fc76f86c29b3ee2c531cc10fba9ada9d86f8
Kristoffermg <33570390+Kristoffermg@users.noreply.github.com>
2023-05-04 18:13:53 +0200

    Outlier elimination
    -> added z-score
    -> added MAPE (mean absolute percentage error) in the calculation of errors

--- Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/ModelExecution/TFmain/lineartest.py
+++ Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/ModelExecution/TFmain/lineartest.py
@@ -1,16 +1,23 @@
 import pandas
 import numpy
 from sklearn.model_selection import train_test_split
-from DataHandling.datatransformerproduction import TransformData
+from DataHandling.dataFilter import DataFilter
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error 
+from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as p
 from scipy import stats
 import warnings
+import os
 warnings.filterwarnings("ignore")
 
+cwd = os.getcwd()
+
+# data = DataFilter.fetch(None, cwd + "\Data\ETTh1.csv", "2018-06-19 19:00:00", "2018-06-26 19:00:00")
+# DataFilter.execute(None, data, r"C:\Users\krist\source\repos\CAOS calc\Transformer-Forecasting\Transformer-Forecasting\Transformer-Forecasting-Web\Transformer-Forecasting-Web")
+# dataframe = pandas.read_csv(".\Data\cleandata.csv")
 dataframe = pandas.read_csv(".\Data\outliertest.csv")
 dataframe.drop("HUFL", inplace=True, axis=1)
 dataframe.drop("HULL", inplace=True, axis=1)
@@ -23,43 +30,36 @@ dataframe.plot(x="date", y="OT", kind="line")
 
 # p.show()
 
-OT_values = numpy.asarray(dataframe["OT"])
-mean = numpy.mean(OT_values)
-std = numpy.std(OT_values)
 
-print('mean of the dataset is', mean)
-print('std. deviation is', std)
-
-# threshold = 1.7
-# no_outliers = []
-# for i in OT_values:
-#     z = (i-mean)/std
-#     if z < threshold:
-#         no_outliers.append(i)
-
-dataframe = dataframe[(numpy.abs(stats.zscore(dataframe["OT"])) < 1.5)]
+dataframe = dataframe[(numpy.abs(stats.zscore(dataframe["OT"])) < 2)]
 print(f"Rows: {len(dataframe)}")
 
+# 'year', 'month', 'day', 'hour', 'weekday', 'weekofyear', 'quarter'
 dataframe['datetime'] = pandas.to_datetime(dataframe['date'])
 dataframe = dataframe.apply(lambda row: pandas.Series({
-                                                                    "day": row.datetime.day,
-                                                                    "month": row.datetime.month, 
+                                                                    # "year":row.datetime.year,
+                                                                    # "month":row.datetime.month,
                                                                     "hour":row.datetime.hour,
+                                                                    # "day":row.datetime.day,
                                                                     "weekday":row.datetime.weekday(),
-                                                                    "quarter": row.datetime.quarter,
+                                                                    # "quarter":row.datetime.quarter,
                                                                     'OT': row.OT}), axis=1)
 
+numberOfColumns = dataframe.shape[1] - 1
+x_columns = []
+for i in range(numberOfColumns):
+    x_columns.append(i)
 
 scaler = MinMaxScaler()
 normalizedDataframe = scaler.fit_transform(dataframe)
 
 # saving the scaler for the oil temperature for later use when it has to be inversed
 OTScaler = MinMaxScaler()
-OTScaler.min_,OTScaler.scale_=scaler.min_[5],scaler.scale_[5]
+OTScaler.min_,OTScaler.scale_=scaler.min_[numberOfColumns],scaler.scale_[numberOfColumns]
 
 # [:, 0] means 'selecting the first column' and so forth..
-X_values = normalizedDataframe[:, [0, 1, 2, 3, 4]]
-y_values = normalizedDataframe[:, [5]]
+X_values = normalizedDataframe[:, x_columns]
+y_values = normalizedDataframe[:, [numberOfColumns]]
 
 X_train, X_test, y_train, y_test = train_test_split(X_values, y_values, test_size=0.2, random_state=42)   
 
@@ -72,10 +72,10 @@ predictions = model.predict(X_test)
 inversedPrediction = numpy.round(OTScaler.inverse_transform(predictions), 2)
 inversedYtest = numpy.round(OTScaler.inverse_transform(y_test), 2)
 
-# index = 0
-# for thing in y_test:
-#     print(f"{inversedYtest[index][0]} and {inversedPrediction[index][0]}")
-#     index +=1
+index = 0
+for thing in y_test:
+    print(f"{inversedYtest[index][0]} and {inversedPrediction[index][0]}")
+    index +=1
 
 
 # index = 0
@@ -83,7 +83,6 @@ inversedYtest = numpy.round(OTScaler.inverse_transform(y_test), 2)
 #     print(f"{y_test[index][0]} and {predictions[index][0]}")
 #     index +=1
 
-print('MSEinversed: ', mean_squared_error(inversedYtest, inversedPrediction))
-print('MSE: ', mean_squared_error(y_test, predictions))
+print('MSE inversed: ', mean_squared_error(inversedYtest, inversedPrediction))
 print('MAE inversed: ', mean_absolute_error(inversedYtest, inversedPrediction))
-
+print('MSE %: ', mean_absolute_percentage_error(inversedYtest, inversedPrediction))
