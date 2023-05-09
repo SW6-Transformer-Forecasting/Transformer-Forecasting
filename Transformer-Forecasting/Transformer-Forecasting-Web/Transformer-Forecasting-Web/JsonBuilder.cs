532ba243cac02d9121fd33e5a122cc5c6bafee05
Archdragon79 <plaursen7.pl@gmail.com>
2023-05-09 16:54:07 +0200

    added base for Json builder

---
+++ Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/JsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class JsonBuilder
+{
+    List<Object> parameters = new List<Object>();
+
+    void AddParam(Object param)
+    {
+        parameters.Add(param);
+    }
+
+    void BuildJson()
+    {
+        var jsonSetting = JsonConvert.SerializeObject(parameters, Formatting.Indented);
+        File.WriteAllText("params.json", jsonSetting);
+    }
+}
