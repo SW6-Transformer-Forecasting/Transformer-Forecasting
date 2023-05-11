1c2a07fa9b69b1fd29c6e0845e9d444db9eff5c3
Julian <julian.larsen07@gmail.com>
2023-05-11 12:27:16 +0200

    somehow, it can run... but not value in graph

--- Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/Data/Prediction.cs
+++ Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/Data/Prediction.cs
@@ -3,16 +3,29 @@
     public class Prediction
     {
         public Prediction(int id, string model, string periodDescription,
-            DateTime? startDate, DateTime? endDate)
+            DateTime? startDate, DateTime? endDate, double[]? dTP_Value, double[]? lFP_Value)
         {
             Id = id;
             Model = model;
             PeriodDescription = periodDescription;
             StartDate = startDate;
             EndDate = endDate;
+            DTP_Value = dTP_Value;
+            LFP_Value = lFP_Value;
         }
 
         public Prediction() { }
+
+        public Prediction(Prediction prediction) 
+        { 
+            this.Id = prediction.Id;
+            this.Model = prediction.Model;
+            this.PeriodDescription = prediction.PeriodDescription;
+            this.StartDate = prediction.StartDate;
+            this.EndDate = prediction.EndDate;
+            this.DTP_Value = prediction.DTP_Value;
+            this.LFP_Value = prediction.LFP_Value;
+        }
 
         public int Id { get; }
         public string Model { get; }
@@ -21,6 +34,8 @@
         public TimeSpan? StartTime { get; }
         public DateTime? EndDate { get; }
         public TimeSpan? EndTime { get; }
+        public double[]? DTP_Value { get; }
+        public double[]? LFP_Value { get; }
 
 
     }
