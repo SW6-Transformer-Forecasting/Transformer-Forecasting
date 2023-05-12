394083786886f653a5d41f9b14550a205e5ecba8
Kristoffermg <33570390+Kristoffermg@users.noreply.github.com>
2023-05-12 12:10:09 +0200

    progress 1

--- Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/ModelExecution/TFmain/DataHandling/datahandlerproduction.py
+++ Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/ModelExecution/TFmain/DataHandling/datahandlerproduction.py
@@ -1,8 +1,7 @@
 from DataHandling.linearmodeldataproduction import ModelDataProduction
-from DataHandling.datatransformerproduction import TransformData
+from readJSONParams import JsonParams
 import pandas
 import numpy
-import json
 import warnings
 from datetime import datetime, timedelta
 import os
@@ -26,46 +25,23 @@ class DataHandler:
     trainDataInformation = []
     predictDataAsDatetime = []
     linearModelInformation = []
+    dateValues = JsonParams.GetDateValues()
     
     def __init__(self, periodDescription, hoursToPredictAhead, dataTransformer):
         self.SetupTrainPredictData(periodDescription, dataTransformer)
-
-    def ConvertPredictValuesToDatetime(self, startPredictDate, endPredictDate):
-        startPredictAsDatetime = self.ConvertStringToDatetime(startPredictDate)
-        endPredictDateAsDatetime = self.ConvertStringToDatetime(endPredictDate)
-
-        self.predictDataAsDatetime = [startPredictAsDatetime, endPredictDateAsDatetime]
-        
-    def GetTrainPeriodData(self, startPredictDate):
-        trainFrom = self.SubtractOneMonthFromDate(startPredictDate)
-        trainTo = startPredictDate
-        self.trainDataInformation = [trainFrom, trainTo]
-
-    def SubtractOneMonthFromDate(self, startPredictDate):
-        startPredictDateMinusOneYear = self.ConvertStringToDatetime(startPredictDate) - timedelta(days=30)
-
-        if(str(startPredictDateMinusOneYear) < EARLIEST_TRAIN_DATE):
-            startPredictDate = EARLIEST_TRAIN_DATE
-        
-        return str(startPredictDateMinusOneYear)
     
     def ConvertStringToDatetime(self, dateString):
         return datetime.strptime(dateString, "%Y-%m-%d %H:%M:%S")
             
     def SetupTrainPredictData(self, periodDescription, dataTransformer):    
-
-        # data['datetime'] = pandas.to_datetime(data['date'])
-                    
         # gets the training data from the outlier-filtered dataset
-        trainingData = pandas.read_csv(cwd + "\ModelExecution\TFmain\Data\ETTh1OutliersRemoved.csv", usecols=["month", "day", "hour", "quarter", "OT"])
-        print(trainingData)
+        
+        trainingData = pandas.read_csv(cwd + "\ModelExecution\TFmain\Data\ETTh1OutliersRemoved.csv", usecols=self.dateValues + ["OT"])
         numberOfColumns = trainingData.shape[1] - 1
         x_columns = []
         for i in range(numberOfColumns):
             x_columns.append(i)
-        print("BEFORE")
-        print(trainingData)
-        # normalizes the data
+            
         normalizedTrainingData = dataTransformer.FitAndTransformData(trainingData)
 
         # Possible features: 'year', 'month', 'day', 'hour', 'weekday', 'weekofyear', 'quarter'
@@ -77,9 +53,7 @@ class DataHandler:
         
         x_predict = pandas.DataFrame(datePredictValues)
         x_predict.columns = ['date']
-        x_predict = self.ApplyDateValuesSplit(x_predict, False)
-        print("AFTER")
-        print(x_predict)
+        x_predict = self.ApplyDateValuesSplit(x_predict)
         x_predict = dataTransformer.TransformData(x_predict)
         dataTransformer.SaveOTScaler(numberOfColumns)
 
@@ -89,17 +63,13 @@ class DataHandler:
         self.linearModelInformation = ModelDataProduction(periodDescription, x, y, x_predict)
         
         
-    # Handles the date split depending on if the data is the training data or the input to predict on
-    def ApplyDateValuesSplit(self, data, isTrainingData):
+    # Sets the input dates up for normalization and prediction
+    def ApplyDateValuesSplit(self, data):
         data['datetime'] = pandas.to_datetime(data['date'])
-        if(isTrainingData):
-            return data.apply(lambda row: pandas.Series({"month":row.datetime.month, 
-                                                         "day":row.datetime.day, 
-                                                         "hour":row.datetime.hour, 
-                                                         "quarter":row.datetime.weekday(),
-                                                         "OT": row.OT}), axis=1)
-        else:
-            return data.apply(lambda row: pandas.Series({"month":row.datetime.month,
+        
+        
+        
+        return data.apply(lambda row: pandas.Series({"month":row.datetime.month,
                                                          "day":row.datetime.day, 
                                                          "hour":row.datetime.hour,   
                                                          "quarter":row.datetime.quarter,
@@ -108,7 +78,7 @@ class DataHandler:
 
     # Loops through the start date to the end date hourly and returns an array of their string representations
     def GetDatePredictValues(self, dateToPredictFrom):
-        dateToPredictFrom = datetime.strptime(dateToPredictFrom, "%Y-%m-%d %H:%M:%S")
+        dateToPredictFrom = self.ConvertStringToDatetime(dateToPredictFrom)
         dates = []
         for i in range(24):
             dateToPredictFrom += timedelta(hours=1)
---
+++ Transformer-Forecasting/Transformer-Forecasting-Web/Transformer-Forecasting-Web/ModelExecution/TFmain/DataHandling/readJSONParams.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+cwd = os.getcwd()
+
+jsonFile = open(r"C:\Users\krist\source\repos\CAOS calc\Transformer-Forecasting\Transformer-Forecasting\Transformer-Forecasting-Web\Transformer-Forecasting-Web\params.json")
+# jsonFile = open(cwd + "\params.json")
+
+data = json.load(jsonFile)
+
+HUFL = data[0]["Item2"]
+MUFL = data[1]["Item2"]
+LUFL = data[2]["Item2"]
+HULL = data[3]["Item2"]
+MULL = data[4]["Item2"]
+LULL = data[5]["Item2"]
+LearningRate = data[6]["Item2"]
+Year = data[7]["Item2"]
+Month = data[8]["Item2"]
+Day = data[9]["Item2"]
+Hour = data[10]["Item2"]
+Weekday = data[11]["Item2"]
+Weekofyear = data[12]["Item2"]
+Quarter = data[13]["Item2"]
+DTPswitch = data[14]["Item2"]
+HoursToPredict = data[15]["Item2"]
+
+class JsonParams(): 
+    def GetDateValues(self):
+        dateValues = []
+        if(Year): dateValues.append("year")
+        if(Month): dateValues.append("month")
+        if(Day): dateValues.append("day")
+        if(Hour): dateValues.append("hour")
+        if(Weekday): dateValues.append("weekday")
+        if(Weekofyear): dateValues.append("weekofyear")
+        if(Quarter): dateValues.append("quarter")
+        
+        return dateValues
+    
+test = JsonParams()
+print(test.GetDateValues() + ["OT"])
